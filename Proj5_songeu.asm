7f3f44eadc0d8337d74332e3340bc8b67ca9944c
Eugene Song <songeu@oregonstate.edu>
2022-02-23 15:35:09 -0800

    Add project files.

---
+++ Proj5_songeu.asm
@@ -0,0 +1,31 @@
+TITLE Arrays, Addressing, and Stack-Passed Parameters     (Proj5_songeu.asm)
+
+; Author: Eugene Song
+; Last Modified: February 23, 2022
+; OSU email address: songeu@oregonstate.edu
+; Course number/section:   CS271 Section 400
+; Project Number: 5             Due Date: February 27, 2022
+; Description: This file is provided as a template from which you may work
+;              when developing assembly projects in CS271.
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
