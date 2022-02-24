aa3a62fef82946086c7faa2ab973a24599ccd8da
Eugene Song <songeu@oregonstate.edu>
2022-02-23 23:58:32 -0800

    -EAX has randomNum -work on storing into array

--- Proj5_songeu.asm
+++ Proj5_songeu.asm
@@ -20,9 +20,9 @@ HI = 50
 .data
 
 	intro1			BYTE	"The Birth of Sorted Random Number Integers! By Eugene",13,10,13,10, 0
-	intro2			BYTE	"This program generates 200 random numbers in the range [15 ... 50], displays the ",13,10
-					BYTE	"original list, sorts the list, displays the median value of the list, displays the ",13,10
-					BYTE	"list sorted in ascending order, then displays the number of instances of each ",13,10 
+	intro2			BYTE	"This program's objective is to pump out 200 random integers from 15 through 50. It will display ",13,10
+					BYTE	"and sort the original list containing the integers, display its median value, then rearrange ",13,10
+					BYTE	"and output the list in ascending order and finally show the number of instances of each ",13,10 
 					BYTE	"generated value, starting with the number of 10s.",13,10,13,10,0
 	unsorted_label	BYTE	"Your unsorted random numbers:",13,10, 0
 	median_label	BYTE	"The median value of the array: ", 13,10,0 
@@ -30,18 +30,23 @@ HI = 50
 	instance_label	BYTE	"Your list of instances of each generated number, starting with the number of 10s:",13,10, 0
 	farewell_msg	BYTE	"Goodbye, and thanks for coming!", 13,10, 0 
 
+	someArray		DWORD	ARRAYSIZE DUP (?)	
+	arrayCount		DWORD	LENGTHOF someArray	; count = 200
+
 
 
 .code
 main PROC
+	CALL	Randomize			; random seed 
+	
+	PUSH	OFFSET intro1		; push intro1 to stack
+	PUSH	OFFSET intro2		; push intro2 to stack
+	CALL	introduction
 
-;	CALL Randomize			; random seed
+	PUSH	arrayCount			
+	PUSH	OFFSET someArray	; push empty array onto stack
+	CALL	fillArray
 
-	PUSH	OFFSET intro1	; push intro1 to stack
-	PUSH	OFFSET intro2	; push intro2 to stack
-	CALL introduction
-
-	; fillArray
 	; sortList
 	; exchangeElements
 	; displayMedian 
@@ -54,20 +59,64 @@ main ENDP
 
 introduction PROC
 
-	PUSH  EBP				; Step 1) Preserve EBP
-	MOV   EBP, ESP			; Step 2) Assign static stack-frame pointer
+	PUSH	EBP				; Step 1) Preserve EBP
+	MOV		EBP, ESP		; Step 2) Assign static stack-frame pointer
 
-	MOV		EDX, [ESP+12]	; move +12 bytes from ESP to store intro1 
+	MOV		EDX, [ESP+12]	; move +12 bytes from ESP to store intro1  [base + offset] 
 	CALL	WriteString
 	xor		EDX, EDX
-	mov		EDX, [ESP+8]	; move +8 bytes from ESP to store intro2
+	mov		EDX, [ESP+8]	; move +8 bytes from ESP to store intro2	[base + offset]
 	CALL	WriteString
 	xor		EDX, EDX
 	
 	pop		EBP
-
-	RET		8
+	RET		8				; clears pre-call introduction parameters [stack pointer reset]
 
 introduction ENDP
+
+fillArray PROC
+
+	PUSH	EBP				; Step 1) Preserve EBP
+	MOV		EBP, ESP		; Step 2) Assign static stack-frame pointer
+
+	; generate random numbers
+
+;	MOV		ECX, 100
+;_loopMe:
+
+	MOV		EAX, HI			; upper bound
+	ADD		EAX, 1			; EAX + 1 to include upper range
+	CALL	RandomRange		; takes EAX (upper); outputs new EAX 
+	MOV		EBX, LO
+	PUSH	EAX				; store seed value --> stack
+	MOV		EAX, HI
+	ADD		EAX, 1			; to include upper range again
+	SUB		EAX, EBX		; upper - lower --> EAX
+
+		; switch EAX / EBX to prepare for div
+	MOV		EBX, EAX
+	POP		EAX				; popped off seed --> EAX 
+
+		; EAX (seed) / EBX (upper-lower) at this point
+	XOR		EDX, EDX
+	DIV		EBX				; randomNum = seed % (upper-lower) + lower
+
+	MOV		EBX, LO
+	ADD		EDX, EBX		
+		; at this point, EDX has the randomNum btwn LO & HI
+
+	MOV		EAX, EDX		; EAX has randomNum btwn LO & HI
+	CALL	WriteDec
+	CALL	CrLf
+	; store into array
+
+;	LOOP _loopMe
+	
+
+		; fill in array
+		; LOOP counter
+		; compare with length
+
+fillArray ENDP
 
 END main
